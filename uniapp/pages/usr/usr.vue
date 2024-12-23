54ee203d40e16e0fa49e6ed909d792256aaa0c6b
ChenMO <140606766+Chenmo007@users.noreply.github.com>
2024-12-23 14:19:29 +0800

    Add files via upload
    文件作用：
    usr.vue文件是UniApp项目中的一个Vue组件，其主要作用是根据用户的登录状态来决定展示登录组件还是用户信息组件。以下是它的一些具体作用：
    
    状态判断与组件切换：
    通过计算属性token从Vuex状态管理中获取用户的登录状态。
    使用v-if和v-else指令根据token的存在与否来决定渲染<my-login>组件还是<my-userinfo>组件。
    如果用户未登录（token不存在），则展示<my-login>组件，允许用户进行登录操作。
    如果用户已登录（token存在），则展示<my-userinfo>组件，显示用户的相关信息。
    
    用户交互入口：
    为用户提供了一个交互的入口，根据用户的登录状态引导用户进行相应的操作。
    对于未登录的用户，提供登录的入口，使用户能够登录并访问应用的更多功能。
    对于已登录的用户，展示用户信息，用户可以查看自己的资料或进行其他与用户相关操作。
    
    简化页面逻辑：
    将登录和用户信息展示的逻辑封装在不同的组件中，通过简单的条件判断来切换展示的组件。

---
+++ uniapp/pages/usr/usr.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    <my-login v-if="!token" />
+    <my-userinfo v-else />
+  </view>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState('m_user', ['token']),
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+/* Add your styles here */
+</style>
